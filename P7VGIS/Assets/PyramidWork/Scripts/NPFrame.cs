07f181ea80fc173baba537136cba00b87c544706
Miberen <miberen@hotmail.com>
2015-11-02 14:39:49 +0100

    Work work, nothing new yet,.

--- P7VGIS/Assets/PyramidWork/Scripts/NPFrame.cs
+++ P7VGIS/Assets/PyramidWork/Scripts/NPFrame.cs
@@ -3,95 +3,115 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class NPFrame : MonoBehaviour {
+public class NpFrame : MonoBehaviour {
 
-    #region Publics
+    #region Public Properties
+
+    public static List<NpFrame> MasterList
+    {
+        get { return _masterList; }
+    }
+
+    #endregion
+
+
+    #region Private Properties
+
+    //Shader containing pyramid functions
+    private ComputeShader cSMain;
+
+    //The master of lists
+    private static List<NpFrame> _masterList = new List<NpFrame>();
+
+    //Creates the nescessary RenderTextures
+    private List<RenderTexture> _analyzeList = new List<RenderTexture>();
+    private List<List<RenderTexture>> _synthesizeLists = new List<List<RenderTexture>>();
+    private RenderTexture _source;
+    private RenderTexture _done;
+    private RenderTexture _donePow2;
+    private RenderTexture _depth;
+
+    //Bool to see if textures are initialized
+    private bool _isInit = false;
+
+    //How many analyze levels there are
+    private int _levels;
+    // To check if levels has changed
+    private int _lastLevels;
+
+    //The size of the screen last frame;
+    private Vector2 _lastScreenSize;
+
+    //List of power of 2s
+    private List<int> _pow2S = new List<int>();
+    #endregion
+
+
+    #region Constructors
+
+    [Obsolete("Please use contructor with int for now")]
+    public NpFrame()
+    {
+        if (CheckCompatibility())
+        {
+            BasicInits();
+        }
+    }
+
+    public NpFrame (int analyzeLevels)
+    {
+        if(CheckCompatibility())
+        {
+            _levels = analyzeLevels;
+            BasicInits();
+        }      
+    }
+
+    [Obsolete("Please use contructor with int for now")]
+    public NpFrame(Vector2 analyzeRes)
+    {
+        if (CheckCompatibility())
+        {
+            BasicInits();
+        }
+    }
+
+    #endregion
+
+
+    #region Public Functions
+
     /// <summary>
     /// Sets the source texture for the pyramid.</summary>
     /// <param name="src"> The source RenderTexture use for analyzation pyramid.</param>
     public void SetSrcTex(ref RenderTexture src)
     {
-        source = src;
-        isInit = false;
+        _source = src;
+        _isInit = false;
     }
-
-    List<RenderTexture> GetSynthesis(Vector2 fromResolution)
+    /// <summary>
+    /// Generates a Synthesis from the specified power of 2 resolution.</summary>
+    /// <param name="fromResolution"> The analysis resolution to synthesize from.</param>
+    public List<RenderTexture> GetSynthesis(Vector2 fromResolution)
     {
-
         return null;
     }
 
+
     #endregion
 
-
-    #region Privates
-
-    //Shader containing pyramid functions
-    private ComputeShader cSMain;
-    
-    //The master of lists
-    private static List<NPFrame> masterList = new List<NPFrame>();
-
-    //Creates the nescessary RenderTextures
-    private List<RenderTexture> analyzeList = new List<RenderTexture>();
-    private List<List<RenderTexture>> synthesizeLists = new List<List<RenderTexture>>();
-    private RenderTexture source;
-    private RenderTexture done;
-    private RenderTexture donePow2;
-    private RenderTexture depth;
-
-    //Bool to see if textures are initialized
-    private bool isInit = false;
-
-    //How many analyze levels there are
-    private int levels;
-    // To check if levels has changed
-    private int lastLevels;
-
-    //The size of the screen last frame;
-    private Vector2 lastScreenSize;
-
-    //List of power of 2s
-    private List<int> pow2s = new List<int>();
-    #endregion
-
-    #region Constructors
-
-    public NPFrame()
+    private void BasicInits()
     {
-        if (CheckCompatibility())
-        {
-            basicInits();
-        }
-    }
-
-    public NPFrame (int analyzeLevels)
-    {
-        if(CheckCompatibility())
-        {
-            basicInits();
-        }      
-    }
-
-    public NPFrame(Vector2 analyzeRes)
-    {
-        if (CheckCompatibility())
-        {
-            basicInits();
-        }
-    }
-    #endregion
-
-    private void basicInits()
-    {
-        masterList.Add(this);
+        _masterList.Add(this);
         GenPow2();
+        
+        cSMain = (ComputeShader)Resources.Load("NPFrame/Shaders/NPFrame");
     }
 
-    private void InitAnalyze(RenderTexture source, int levels)
+    private void InitAnalyze(RenderTexture src, int levels)
     {
         // These loops to handle changing levels size dynamically works a bit iffy ( I.E Doesnt fucking work ATM, memleak)
-        foreach (RenderTexture rT in analyzeList)
+        foreach (RenderTexture rT in _analyzeList)
         {
             if (rT.IsCreated())
             {
@@ -100,49 +120,50 @@ public class NPFrame : MonoBehaviour {
             }
         }      
 
-        analyzeList.Clear();
+        _analyzeList.Clear();
 
-        int size = NextPow2(new Vector2(source.width, source.height));
+        int size = NextPow2(new Vector2(src.width, src.height));
         for (int i = 0; i < levels; i++)
         {
-            analyzeList.Add(new RenderTexture(pow2s[pow2s.IndexOf(size) - i], pow2s[pow2s.IndexOf(size) - i], 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear));
-            analyzeList[i].enableRandomWrite = true;
-            analyzeList[i].Create();
+            _analyzeList.Add(new RenderTexture(_pow2S[_pow2S.IndexOf(size) - i], _pow2S[_pow2S.IndexOf(size) - i], 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear));
+            _analyzeList[i].enableRandomWrite = true;
+            _analyzeList[i].Create();
         }
+
+
 
         // TODO: Maybe move?
         // hack 4 lyfe
-        donePow2 = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
-        donePow2.enableRandomWrite = true;
-        donePow2.Create();
+        _donePow2 = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        _donePow2.enableRandomWrite = true;
+        _donePow2.Create();
 
-        done = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
-        done.enableRandomWrite = true;
-        done.Create();
+        _done = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        _done.enableRandomWrite = true;
+        _done.Create();
 
-        lastScreenSize = new Vector2(Screen.width, Screen.height);
-        lastLevels = levels;
-        isInit = true;
-    }
-
-
-    private void Awake()
-    {
-        cSMain = (ComputeShader)Resources.Load("NPFrame");
+        _lastScreenSize = new Vector2(Screen.width, Screen.height);
+        _lastLevels = levels;
+        _isInit = true;
     }
 
     // Use this for initialization
-    private void Start ()
+    void Start ()
     {
-        InitAnalyze(source, levels);
+        cSMain = (ComputeShader)Resources.Load("NPFrame/Shaders/NPFrame");
+        Debug.Log(cSMain.name);
+
+        InitAnalyze(_source, _levels);
 	}
 	
 	// Update is called once per frame
-	private void Update ()
+	void Update ()
     {
         //Check if the temp texture isn't initialized or the screen size has changed requiring a new size texture, if it has then reinit them // TODO: fix last levels change( dem mem leaks)
-        if (!isInit || lastScreenSize != new Vector2(Screen.width, Screen.height))
-            InitAnalyze(source, levels);
+        if (!_isInit || _lastScreenSize != new Vector2(Screen.width, Screen.height))
+            InitAnalyze(_source, _levels);
+
+        Analyze(_levels);
     }
 
     #region Helper Functions
@@ -152,21 +173,21 @@ public class NPFrame : MonoBehaviour {
     {
         for (int i = 1; i < 20 + 1; i++)
         {
-            pow2s.Add((int)Mathf.Pow(2, i));
+            _pow2S.Add((int)Mathf.Pow(2, i));
         }
     }
 
     /// <summary>
     /// Finds the power of 2 image which will fit the source image e.g ( 951x100 -> 1024x1024 ).</summary>
-    /// <param name="source"> The source RenderTexture to find a fit for.</param>
+    /// <param name="resolution"> The source resolution to find a fit for.</param>
     private int NextPow2(Vector2 resolution)
     {
         int biggest = (int)Mathf.Max(resolution.x, resolution.y);
         int final = 0;
 
-        foreach (int i in pow2s)
+        foreach (int i in _pow2S)
         {
-            if (biggest > i) final = pow2s[pow2s.IndexOf(i) + 1];
+            if (biggest > i) final = _pow2S[_pow2S.IndexOf(i) + 1];
         }
         return final;
     }
@@ -182,22 +203,21 @@ public class NPFrame : MonoBehaviour {
             return SystemInfo.supportsComputeShaders;
     }
     #endregion
-
+    
     #region Compute Shader Calls
 
     void Analyze(int levels)
     {
         for (int i = 0; i < levels - 1; i++)
         {
-            cSMain.SetTexture(cSMain.FindKernel("Analyze"), "source", analyzeList[i]);
-            cSMain.SetTexture(cSMain.FindKernel("Analyze"), "dest", analyzeList[i + 1]);
+            cSMain.SetTexture(cSMain.FindKernel("Analyze"), "source", _analyzeList[i]);
+            cSMain.SetTexture(cSMain.FindKernel("Analyze"), "dest", _analyzeList[i + 1]);
 
-            cSMain.Dispatch(cSMain.FindKernel("Analyze"), (int)Mathf.Ceil(analyzeList[i + 1].width / 32), (int)Mathf.Ceil(analyzeList[i + 1].height / 32), 1);
+            cSMain.Dispatch(cSMain.FindKernel("Analyze"), (int)Mathf.Ceil(_analyzeList[i + 1].width / 32), (int)Mathf.Ceil(_analyzeList[i + 1].height / 32), 1);
         }
-        synthesizeList[0] = analyzeList[levels - 1];
     }
 
-    void Synthesize(int levels)
+    void Synthesize(RenderTexture src, int levels)
     {
         for (int i = 0; i < levels - 1; i++)
         {
@@ -209,5 +229,7 @@ public class NPFrame : MonoBehaviour {
     }
 
     #endregion
+    
+
 
 }
--- P7VGIS/Assets/PyramidWork/Scripts/TestImplement.cs
+++ P7VGIS/Assets/PyramidWork/Scripts/TestImplement.cs
@@ -3,16 +3,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class TestImplement : MonoBehaviour {
+public class TestImplement : MonoBehaviour
+{
 
+
+    private NpFrame frame;
 	// Use this for initialization
-	void Start () {
-        NPFrame frame = new NPFrame(5);     
 
-        
+    void Start ()
+    {
+        frame = new NpFrame(5);
 	}
-	
 
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        frame.SetSrcTex(ref source);
+        List<RenderTexture> derp = frame.GetSynthesis(new Vector2(32, 32));
+    }
 	// Update is called once per frame
 	void Update () {
 	
